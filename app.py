03df6e73f22483c15e25c5c2af954bc87d84763a
Ashwani Kumar Tiwari <43306519+Ashwani-kumar-Tiwari@users.noreply.github.com>
2023-01-09 15:52:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import os
+#import os
 from flask import Flask
-from azure.monitor.opentelemetry.exporter import AzureMonitorMetricExporter
+#from azure.monitor.opentelemetry.exporter import AzureMonitorMetricExporter
 app = Flask(__name__)
 
-exporter = AzureMonitorMetricExporter(
-    connection_string=os.environ["InstrumentationKey=b2ee777e-d3d1-4bd9-aa4f-d3c58d2b0a64"]
-)
+#exporter = AzureMonitorMetricExporter(
+#    connection_string=os.environ["InstrumentationKey=b2ee777e-d3d1-4bd9-aa4f-d3c58d2b0a64"]
+#)
 
 @app.route("/")
 def home():
