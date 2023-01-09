841043d0b2d59f3f0bfcbc60224caf54d4709ac6
Ashwani Kumar Tiwari <tiwariashwani3012@gmail.com>
2023-01-09 14:30:52 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from azure.monitor.opentelemetry.exporter import AzureMonitorMetricExporter
+app = Flask(__name__)
+
+exporter = AzureMonitorMetricExporter(
+    connection_string=os.environ["InstrumentationKey=b2ee777e-d3d1-4bd9-aa4f-d3c58d2b0a64"]
+)
+
+@app.route("/")
+def home():
+    return "Hello, this is a sample Python Web App running on Flask Framework!!!"
+
+if __name__ == "__main__":
+    app.run()
